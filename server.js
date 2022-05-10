144e57b6698f13a2b23b86b8a64f49adf4a71951
Karina Oh <eunjae.karina@gmail.com>
2022-05-09 21:56:48 -0700

    update 2

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ let app = http.createServer(
     function (request,response){
        request.addListener ('end',
             function(){
-                files.serve(request,response);
+                file.serve(request,response);
             }
             
         ).resume();
